dd2bdebceb9cd315b8380a8e08b63d6a4675efae
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-07-27 00:13:58 +0900

    [solved] 0727 3_2 stack_min

--- Chap3/2_Stack_Min.py
+++ Chap3/2_Stack_Min.py
@@ -3,7 +3,6 @@ class Stack():
     def pop(self):
         if len(self.stack) > 0 :
             result = self.stack.pop()
-
         else:
             result = None
             print("Out of range")
@@ -23,26 +22,19 @@ class Stack():
             return False
 
     def min(self):
-        temp_stack = Stack()
-        min = stack.pop()
-        temp_stack.push(min)
-
-        while len(stack.stack) == 0:
-            min_a = stack.pop()
-            temp_stack.push(min_a)
+        temp = []
+        min = 999
+        for _ in range(len(self.stack)):
+            min_a = self.stack.pop()
+            temp.append(min_a) #새로 팝한 애가 min
             if min > min_a:
                 min = min_a
 
-        while len(temp_stack.stack) == 0:
-            stack.push(temp_stack.pop())
+        for _ in range(len(temp)):
+            min_b = temp.pop()
+            stack.push(min_b)
 
         return min
-
-
-
-
-
-
 
 
 stack = Stack()
@@ -52,7 +44,6 @@ stack.push(3)
 print(stack.min())
 stack.push(1)
 print(stack.min())
-stack.push(0)
+stack.push(4)
 print(stack.min())
-
 
