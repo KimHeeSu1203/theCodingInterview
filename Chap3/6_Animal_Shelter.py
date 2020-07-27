568ca9ac5088eb86d2ccd9ed4e4dab4fc22c49a4
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2020-07-27 12:10:35 +0900

    [solve] 0727 3_6 animalShelter

--- Chap3/6_Animal_Shelter.py
+++ Chap3/6_Animal_Shelter.py
@@ -1,15 +1,23 @@
 #먼저 온 애가 먼저 나가야 하니까 FIFO QUEUE
-
 class Queue():
+    order = 0
     dogqueue = []
     catqueue = []
     def dequeueAny(self):
+        if (len(self.dogqueue) > 0) or (len(self.catqueue)>0):
+            firstAnimal = self.dogqueue if self.dogPeek()[0] < self.catPeek()[0] else self.catqueue
+
+        else:
+            return False
+        return firstAnimal.pop(0)
+        """
         longQueue = self.dogqueue if len(self.dogqueue)>len(self.catqueue) else self.catqueue
         if len(longQueue) > 0:
             result = longQueue.pop(0)
         else:
             return False
         return result
+        """
 
     def dequeueDog(self):
         if len(self.dogqueue) > 0:
@@ -23,15 +31,25 @@ class Queue():
 
     def enqueue(self,animal,item):
         if animal == "dog":
-            self.dogqueue.append(item)
+            self.dogqueue.append((self.order,item))
         elif animal == "cat":
-            self.catqueue.append(item)
+            self.catqueue.append((self.order,item))
+        self.order = self.order + 1
 
-    def peek(self):
-        return self.queue[0]
+    def dogPeek(self):
+        return self.dogqueue[0]
 
-    def isEmpty(self):
-        if len(self.queue) == 0:
+    def catPeek(self):
+        return self.catqueue[0]
+
+    def isDogEmpty(self):
+        if len(self.dogqueue) == 0:
+            return True
+        else:
+            return False
+
+    def isCatEmpty(self):
+        if len(self.catqueue) == 0:
             return True
         else:
             return False
@@ -39,6 +57,9 @@ class Queue():
 
 animalShelter = Queue()
 animalShelter.enqueue("dog",1)
+animalShelter.enqueue("cat",7)
+animalShelter.enqueue("cat",9)
+animalShelter.enqueue("cat",8)
 animalShelter.enqueue("dog",2)
 animalShelter.enqueue("dog",3)
 animalShelter.enqueue("dog",4)
@@ -49,7 +70,14 @@ animalShelter.enqueue("cat",9)
 animalShelter.enqueue("cat",10)
 
 print(animalShelter.dequeueAny())
+print(animalShelter.dequeueAny())
+print(animalShelter.dequeueAny())
+print(animalShelter.dequeueAny())
+print(animalShelter.dequeueAny())
+
+"""
 print(animalShelter.dequeueCat())
 print(animalShelter.dequeueDog())
 print(animalShelter.dequeueAny())
 print(animalShelter.dequeueAny())
+"""
